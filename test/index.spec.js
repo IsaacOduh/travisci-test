45d8a6d85f0fb58bc3da1787d1fc1a6d31a6507f
Isaac Oduh <ufuomaoduh@gmail.com>
2018-07-24 12:01:41 +0100

    more code

---
+++ test/index.spec.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const calculator = require('../index');
+
+describe('calculator',() => {
+    describe('add function',() => {
+        it('adds numbers', () => {
+            const result = calculator.add(1,1);
+            assert.equal(result,2);
+        });
+    });
+});
